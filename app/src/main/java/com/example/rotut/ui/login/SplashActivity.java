735e57e055699051e2fd1fe034fd63684283fe8c
RuofanMao <Ruofanm@users.noreply.github.com>
2020-05-04 03:42:00 -0400

    splash page
    adding a splash page and java file

---
+++ app/src/main/java/com/example/rotut/ui/login/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.example.rotut.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.rotut.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Thread myThread = new Thread() {//创建子线程
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);//使程序休眠一秒
+                    Intent it = new Intent(getApplicationContext(), LoginActivity.class);
+                    startActivity(it);
+                    finish();//关闭当前活动
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();//启动线程
+    }
+}

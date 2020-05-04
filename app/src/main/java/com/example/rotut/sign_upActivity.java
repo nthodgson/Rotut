2b397435c96d834852430a340db1c44a02245df6
Nathan <hodgson_nathan@wheatoncollege.edu>
2020-05-04 14:11:38 -0400

    Fix navigation error

---
+++ app/src/main/java/com/example/rotut/sign_upActivity.java
@@ -0,0 +1,13 @@
+package com.example.rotut;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sign_upActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}

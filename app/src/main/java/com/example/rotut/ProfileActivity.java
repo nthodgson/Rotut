a2b6f754295e50315f0a69d335a6534f0ac9c6f6
Nathan <hodgson_nathan@wheatoncollege.edu>
2020-05-06 15:33:49 -0400

    Update profile page back arrow

--- app/src/main/java/com/example/rotut/ProfileActivity.java
+++ app/src/main/java/com/example/rotut/ProfileActivity.java
@@ -2,8 +2,12 @@ package com.example.rotut;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -13,5 +17,17 @@ public class ProfileActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
         getSupportActionBar().hide(); // hide the title bar
         setContentView(R.layout.profile);
+
+        configureNavigation();
+    }
+
+    private void configureNavigation() {
+        ImageButton signUpButton = (ImageButton) findViewById(R.id.backArrow);
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ProfileActivity.this, AccountActivity.class));
+            }
+        });
     }
 }

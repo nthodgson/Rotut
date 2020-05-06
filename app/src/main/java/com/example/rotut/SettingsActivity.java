08988b59ea6de8f6e3d7a54f378729ffb516b76d
RuofanMao <Ruofanm@users.noreply.github.com>
2020-05-06 17:12:55 -0400

    Setting Page Bottom Bar

--- app/src/main/java/com/example/rotut/SettingsActivity.java
+++ app/src/main/java/com/example/rotut/SettingsActivity.java
@@ -1,9 +1,14 @@
 package com.example.rotut;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.Window;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -13,5 +18,28 @@ public class SettingsActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
         getSupportActionBar().hide(); // hide the title bar
         setContentView(R.layout.settings);
+
+
+        BottomNavigationView bottomNavigation;
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
     }
+
+    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.navbar_home:
+                            startActivity(new Intent(SettingsActivity.this, HomeActivity.class));
+                            return true;
+                        case R.id.navbar_account:
+                            startActivity(new Intent(SettingsActivity.this, AccountActivity.class));
+                            return true;
+                        case R.id.navbar_search:
+                            startActivity(new Intent(SettingsActivity.this, SearchActivity.class));
+                            return true;
+                    }
+                    return false;
+                }
+            };
 }

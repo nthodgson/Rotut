54c5ef0f57f137b965d96dbcccf17fd7e6058cab
Zheyu-qin <62949009+Zheyu-qin@users.noreply.github.com>
2020-05-06 13:37:19 -0400

    developing the settings page, make it jump to other pages

--- app/src/main/java/com/example/rotut/SettingsActivity.java
+++ app/src/main/java/com/example/rotut/SettingsActivity.java
@@ -3,6 +3,7 @@ package com.example.rotut;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -27,15 +28,31 @@ public class SettingsActivity extends AppCompatActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
         Switch message;
+        Switch location;
         TextView Check_for_update;
         TextView Q_and_A_center;
         TextView About_us;
+        Button Sign_out;
         Check_for_update=(TextView)findViewById(R.id.textView34);
         Q_and_A_center=(TextView)findViewById(R.id.textView35);
         About_us=(TextView)findViewById(R.id.textView36);
         message= (Switch) findViewById(R.id.switch3);
+        location=(Switch) findViewById(R.id.switch4);
+        Sign_out=(Button) findViewById(R.id.sign_out_button);
         // Make the switch work
         message.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked==true){
+                    Toast.makeText(getBaseContext(),"On",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(getBaseContext(),"Off",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        location.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(isChecked==true){
@@ -68,6 +85,14 @@ public class SettingsActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent Show_about =new Intent(SettingsActivity.this,About_usActivity.class);
                 startActivity (Show_about);
+            }
+        });
+
+        Sign_out.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Sign_out =new Intent(SettingsActivity.this,SignInActivity.class);
+                startActivity (Sign_out);
             }
         });
 

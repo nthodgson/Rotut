fba538bb3ae2749d119994a1e30dc0cf5173450f
Nathan <hodgson_nathan@wheatoncollege.edu>
2020-05-08 19:58:35 -0400

    Update back arrow functionality

--- app/src/main/java/com/example/rotut/NewAccountActivity.java
+++ app/src/main/java/com/example/rotut/NewAccountActivity.java
@@ -2,8 +2,12 @@ package com.example.rotut;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class NewAccountActivity extends AppCompatActivity {
 
@@ -13,5 +17,25 @@ public class NewAccountActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
         getSupportActionBar().hide(); // hide the title bar
         setContentView(R.layout.new_account);
+
+        configureNavigation();
+    }
+
+    private void configureNavigation() {
+        ImageButton backArrow = (ImageButton) findViewById(R.id.backArrow5);
+        backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(NewAccountActivity.this, SignUpActivity.class));
+            }
+        });
+
+        Button createAccountButton = (Button) findViewById(R.id.createAccount2);
+        createAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(NewAccountActivity.this, SignInActivity.class));
+            }
+        });
     }
 }

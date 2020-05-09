7fb697943c4addb47d29326ab28fc15237d4440e
dmitrikorin <korin_Dmitri@wheatoncollege.edu>
2020-05-08 20:27:07 -0400

    Generating Users

--- app/src/main/java/com/example/rotut/SignUpActivity.java
+++ app/src/main/java/com/example/rotut/SignUpActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.rotut.ui.login.LoginActivity;
 
@@ -18,11 +20,18 @@ public class SignUpActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
         getSupportActionBar().hide(); // hide the title bar
         setContentView(R.layout.sign_up);
-
         configureNavigation();
     }
 
     private void configureNavigation() {
+        final EditText fName = (EditText) findViewById(R.id.firstName);
+        final EditText lName = (EditText) findViewById(R.id.lastName);
+        final EditText email = (EditText) findViewById(R.id.emailInput);
+        final EditText passW = (EditText) findViewById(R.id.passwordInput);
+        final EditText rePass = (EditText) findViewById(R.id.confirmNewPassword);
+
+
+
         Button signInButton = (Button) findViewById(R.id.signIn);
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,7 +44,25 @@ public class SignUpActivity extends AppCompatActivity {
         createAccountButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(SignUpActivity.this, NewAccountActivity.class));
+                if(passW.getText().toString().equals(rePass.getText().toString()) &&
+                !fName.getText().toString().equals("") &&
+                !lName.getText().toString().equals("") &&
+                !email.getText().toString().equals("") &&
+                !passW.getText().toString().equals("")) {
+                    Intent intent = new Intent(SignUpActivity.this, NewAccountActivity.class);
+                    intent.putExtra("fName", fName.getText().toString());
+                    intent.putExtra("lName", lName.getText().toString());
+                    intent.putExtra("email", email.getText().toString());
+                    intent.putExtra("pass", passW.getText().toString());
+                    startActivity(intent);
+                }else if(fName.getText().toString().equals("") ||
+                        lName.getText().toString().equals("") ||
+                        email.getText().toString().equals("") ||
+                        passW.getText().toString().equals("")){
+                    Toast.makeText(SignUpActivity.this, "Error: all fields must be filled",Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(SignUpActivity.this, "Error: passwords do not match",Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
